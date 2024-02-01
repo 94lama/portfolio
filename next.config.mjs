b8bd8bf396c63df02d605c71f7215ba3ca6b777c
Riccardo <riccardo.lamalfa94@gmail.com>
2024-02-01 01:06:10 +0100

    fix try for 404 error

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/",
+    basePath: ".",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
     images: {
         remotePatterns: [
             {
               protocol: 'http',
-              hostname: 'http://94lama.github.io',
+              hostname: 'http://94lama.github.io/portfolio',
               port: '',
               //pathname: '/portfolio/**',
             },
