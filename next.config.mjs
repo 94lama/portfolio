de4d04b4f61c763e41cc09ac57ad669f2ca3ac6e
Riccardo <riccardo.lamalfa94@gmail.com>
2024-02-01 14:44:28 +0100

    fix weather and paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //basePath: "/*",
+    // basePath: "",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
     images: {
         remotePatterns: [
             {
-              protocol: 'http',
-              hostname: 'http://94lama.github.io/portfolio',
+              protocol: 'https',
+              hostname: '94lama.github.io',
               port: '',
-              //pathname: '/portfolio/**',
+              pathname: '/portfolio/**',
             },
           ],
         unoptimized: true
