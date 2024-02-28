d3df1a6d064adfd5299b13642a216e0270456c92
Riccardo <riccardo.lamalfa94@gmail.com>
2024-02-28 17:49:10 +0100

    fix:style of about item and removal of certifications (future add)

--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,14 @@ const studies = [
   { title: "Bachelor Degree", detail: "Politecnico di Torino", description: "Archietcture Science", date: "2016" },
 ];
 
+/* const certifications = [
+  { title: "Cybersecurity - DevSecOps Expert", detail: "Aulab srl", description: "Specializing course in Cybersecurity", date: "2024" },
+  { title: "Full Stack developer junior", detail: "Aulab srl", description: "Bootcamp in Full Stack Developing", date: "2023" },
+  { title: "2nd Level Post-grad master", detail: "Politecnico di Torino", description: "Metodi e tecniche per il governo dei territori resilienti", date: "2022" },
+  { title: "Master degree", detail: "Politecnico di Torino", description: "Archietcture for the Sustabnable design", date: "2019" },
+  { title: "Bachelor Degree", detail: "Politecnico di Torino", description: "Archietcture Science", date: "2016" },
+]; */
+
 const classGroups = "flex flex-col justify-center relative bg-purple-100 bg-opacity-75 rounded-3xl p-5 mx-10 md:w-4/6 w-[calc(80%)] backdrop-blur";
 
 /* ///[Component] */
@@ -31,7 +39,7 @@ export default function Home() {
 
       {/* ///[About me] */}
       <div className="min-h-screen pt-12 relative">
-        <div className={classGroups + " lg:w-2/6 top-5 md:top-40 lg:left-40"}>
+        <div className={classGroups + " lg:w-2/6 top-5 md:top-40 lg:left-40 max-w-[600px]"}>
           <h1>About me</h1>
           <h3 className="!font-normal !leading-2 !important">
             I am a Full-stack Developer with expertise in Front-end and Back-end programming, acquired through intensive coding bootcamp training, online coursework and constant practice.<br/><br/>My past experiences allowed me to build also a good knowledge on graphic design, Agile methodologies (SCRUM), collaborative teamwork,  and problem-solving skills.
@@ -45,8 +53,9 @@ export default function Home() {
       {/* ///[Studies] */}
       <CardContainer title="Studies" items={studies} />
 
-      {/* ///[Certifications] */}
-      <CardContainer title="Certifications" items={studies} />
+      {/* ///[Certifications] 
+      <CardContainer title="Certifications" items={certifications} />
+      */}
     </main>
   );
 }
